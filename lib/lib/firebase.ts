be9917d1b458b6dc1425196f2d46ee228b2fa2f7
mithi <mithilesh92357@gmail.com>
2026-05-22 12:00:10 +0530

    fixed firebase env config

--- lib/lib/firebase.ts
+++ lib/lib/firebase.ts
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiZQhXF3_bPh-vavpAbBcf24Vlm5z-0xo",
-  authDomain: "genznext-e93b1.firebaseapp.com",
-  projectId: "genznext-e93b1",
-  storageBucket: "genznext-e93b1.firebasestorage.app",
-  messagingSenderId: "779179898682",
-  appId: "1:779179898682:web:48912db39223c812848203",
-  measurementId: "G-C4YYFPD4YF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:
+    process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
